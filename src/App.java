4401828651a2edb0aa885e457d8d04cf351e8e8a
is13mael <ismaelparramoussaif@gmail.com>
2025-10-25 21:49:34 +0200

    second commit

--- src/App.java
+++ src/App.java
@@ -2,20 +2,20 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Cálculo de una potencia");
         System.out.print("Introduzca la base: ");
-        int base = Integer.parseInt(System.console().readLine()); 
+        int base = Integer.parseInt(System.console().readLine());
         System.out.print("Introduzca el exponente: ");
         int exponente = Integer.parseInt(System.console().readLine());
 
         double resultado = 1.0;
         int valor = Math.abs(exponente);
 
-        for(int i = 0; i < valor; i++){
+        for (int i = 0; i < valor; i++) {
             resultado *= base;
         }
-        if (exponente<0) {
-            resultado = 1.0/resultado;
+        if (exponente < 0) {
+            resultado = 1.0 / resultado;
         }
-        if (exponente == 0){
+        if (exponente == 0) {
             resultado = 1.0;
         }
         System.out.printf("%d^%d = %.5f%n", base, exponente, resultado);
